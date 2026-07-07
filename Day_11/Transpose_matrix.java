7631b8b3976da5d81c63059ff7aae4ead0d347a1
MeghnaDhakad <dhakadmeghna88@gmail.com>
2026-07-07 12:02:08 +0530

    Day 11 commit

---
+++ Day_11/Transpose_matrix.java
@@ -0,0 +1,51 @@
+package Day_11;
+import java.util.*;
+
+public class Transpose_matrix {
+
+    public static int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] ans = new int[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int m = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int[][] result = transpose(matrix);
+
+        System.out.println("Transpose of the matrix:");
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
