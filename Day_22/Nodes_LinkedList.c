e8c73c5fca64a6381e1f87c6ab7b02cc99c5abc6
MeghnaDhakad <dhakadmeghna88@gmail.com>
2026-07-18 10:40:50 +0530

    Day 22

---
+++ Day_22/Nodes_LinkedList.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL;
+    struct Node *temp = NULL;
+    struct Node *newNode = NULL;
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Count nodes
+    int count = 0;
+    temp = head;
+
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
---
+++ Day_22/ReverseLinkedList.java
@@ -0,0 +1,77 @@
+package Day_22;
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            ListNode next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println("Linked List is empty.");
+            return;
+        }
+
+        System.out.println("Enter node values:");
+
+        ListNode head = new ListNode(sc.nextInt());
+        ListNode temp = head;
+
+        for (int i = 1; i < n; i++) {
+            temp.next = new ListNode(sc.nextInt());
+            temp = temp.next;
+        }
+
+        System.out.print("Original Linked List: ");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.print("Reversed Linked List: ");
+        printList(head);
+
+        sc.close();
+    }
+}
